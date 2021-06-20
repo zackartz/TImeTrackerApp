feb11850d4028f9d527d6a40995ab2b556d87f26
zackartz <zackmyers@lavabit.com>
2021-06-19 18:18:50 -1000

    Initial Commit

---
+++ Shared/TimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerApp.swift
+//  Shared
+//
+//  Created by zack on 6/19/21.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
